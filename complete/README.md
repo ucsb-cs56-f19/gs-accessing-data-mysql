5172bb59bf239506b6715b6fe410e550d0874de5
Yinonrousso <yinastyproductions@gmail.com>
2019-10-29 18:01:13 -0700

    yr/all changes for postgres

---
+++ complete/README.md
@@ -0,0 +1,5 @@
+To add records to the database type this at the command line 
+```
+curl -d "id=1&name=yinon&email=yinon@example.org" -X POST http://localhost:8080/demo/add
+```
+
--- complete/src/main/java/com/example/accessingdatamysql/User.java
+++ complete/src/main/java/com/example/accessingdatamysql/User.java
@@ -1,11 +1,13 @@
 package com.example.accessingdatamysql;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
+@Table(name = "\"user\"")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
